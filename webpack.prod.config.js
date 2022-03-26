8641311f81fc05e7df8bd37f153a6e6ec60c8f8e
houzhenyong <mumuhou001@gmail.com>
2022-03-26 22:20:27 +0800

    save

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,20 +2,18 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    // devtool: 'inline-source-map',
     entry: {
-        PI: './src/interpreter/index.ts',
-        test: './test/index.ts'
+        PI: './src/interpreter/index.ts'
     },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         publicPath: '/',    // 如果不设置，输出包的路径前面就没有/符号,找vendor.js的时候会定位错误。
-        // library: {
-        //     name: 'PI',
-        //     type: 'umd',
-        // },
+        library: {
+            name: 'PI',
+            type: 'umd',
+        },
     },
     resolve: {
         extensions: ['.js', '.ts']
